ffdb1349af3b2ff56fc43072102353d9f6272f41
WadeSeidule <wseidule@doximity.com>
2025-01-15 10:57:36 -0500

    update setup

---
+++ scripts/setup.sh
@@ -0,0 +1,33 @@
+# /bin/zsh
+
+# get user input for the zshconfig dir
+echo "Enter the path to the zsh config directory. Default is $HOME/zshconfig"
+read ZSH_CONFIG_DIR
+
+# set the default zsh config dir if the user input is empty
+if [ -z "$ZSH_CONFIG_DIR" ]; then
+    ZSH_CONFIG_DIR="$HOME/zshconfig"
+fi
+
+# check if OMZ is installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is not installed. Installing now..."
+    exit 1
+fi
+
+
+# back up old .zshrc file
+if [ -f "$HOME/.zshrc" ]; then
+    echo "Creating backup of old .zshrc file"
+    mv $HOME/.zshrc $HOME/.zshrc.bak
+fi
+
+# create .zshrc file
+touch $HOME/.zshrc
+
+# add ZSH_CONFIG_DIR to the .zshrc file
+echo "export ZSH_CONFIG_DIR=$ZSH_CONFIG_DIR" >> $HOME/.zshrc
+
+# source zshrc.sh in the .zshrc file
+echo "source $ZSH_CONFIG_DIR/zshrc.sh" >> $HOME/.zshrc
+
